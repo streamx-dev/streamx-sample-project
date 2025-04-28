a557139f177e5938f9d681681639fdbc4a979dda
Jakub Gardo <155527092+jgardo-ds@users.noreply.github.com>
2025-04-28 10:37:47 +0200

    [DXP-1398] Add Windows support (#2)

--- services/web-delivery-service/src/main/java/org/example/project/web/StaticResources.java
+++ services/web-delivery-service/src/main/java/org/example/project/web/StaticResources.java
@@ -5,6 +5,7 @@ import io.vertx.ext.web.Router;
 import io.vertx.ext.web.handler.FileSystemAccess;
 import io.vertx.ext.web.handler.StaticHandler;
 import jakarta.enterprise.event.Observes;
+import java.nio.file.Path;
 import org.eclipse.microprofile.config.inject.ConfigProperty;
 
 public class StaticResources {
@@ -13,7 +14,7 @@ public class StaticResources {
   String directory;
 
   void installRoute(@Observes StartupEvent startupEvent, Router router) {
-    FileSystemAccess handlerVisibility = directory.startsWith("/")
+    FileSystemAccess handlerVisibility = Path.of(directory).isAbsolute()
         ? FileSystemAccess.ROOT : FileSystemAccess.RELATIVE;
     router.route()
         .handler(StaticHandler
